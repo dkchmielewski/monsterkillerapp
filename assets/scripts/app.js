441a2a8491bcbf8b9c566858a0cc03f69b10875f
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-01-01 16:21:15 -0500

    for in loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -174,8 +174,10 @@ function printLogHandler() {
   // console.log(battleLog);
   let i = 0;
   for(const logEntry of battleLog) {
-    console.log(logEntry);
-    console.log(i);
+    console.log(`#${i}`);
+    for(const key in logEntry) {
+      console.log(`${key} => ${logEntry[key]}`);
+    } 
     i++;
   }
 }
